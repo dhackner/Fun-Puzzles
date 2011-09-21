e8f597bab7f662c644c547dc54a0114f442c3bfc
Dan Hackner <dan.hackner@gmail.com>
2011-09-21 01:12:18 -0700

    PEP 8 related naming conventions.

--- AllPossiblePaths.py
+++ AllPossiblePaths.py
@@ -22,28 +22,30 @@ __email__ = "dan.hackner@gmail.com"
 __status__ = "Production"
 
 gridSize = 4
-gridDebug = False#True
+debug = False#True
 
 # DRH | Note to self: This line is equivalent to nesting the list comprehensions,
 # but [['x'] * gridSize] * gridSize DOESN'T work because the outer array
 # doesn't make deep copies of the inner array =-/.
 grid = [(['x'] * gridSize) for x in range(gridSize)]
 
-def numPathsWithPathStepsInEachCell(ingrid, x=0, y=0, stepCounter=1):
-    ingrid[x][y] = stepCounter
-    if gridDebug:
-        printGrid(ingrid)
+def num_paths(ingrid, x=0, y=0, step_counter=1):
+    """Number of total nonintersecting paths from (0,0) to (n,n)."""
+    ingrid[x][y] = step_counter
+    if debug:
+        print_grid(ingrid)
     if x == y == (gridSize-1):
-        if gridDebug:
-            printGrid(ingrid, 1)
+        if debug:
+            print_grid(ingrid, 1)
         return 1
     else:
-        numCombinedPathsFromSurroundingCells = 0
-        for (x2, y2) in getAvailableNextSteps(ingrid, x, y):
-            numCombinedPathsFromSurroundingCells += numPathsWithPathStepsInEachCell(deepcopy(ingrid), x2, y2, stepCounter + 1)
-	return numCombinedPathsFromSurroundingCells
+        total_paths_cell = 0 # Number of combined paths from surrounding cells
+        for (x2, y2) in get_steps(ingrid, x, y):
+            total_paths_cell += num_paths(deepcopy(ingrid), x2, y2, step_counter + 1)
+        return total_paths_cell
 
-def getAvailableNextSteps(g, x, y):
+def get_steps(g, x, y):
+    """Yield all steps from this position that haven't already been used."""
     if x-1 >= 0 and g[x-1][y] == 'x':
         yield (x-1, y)
     if x+1 < len(g) and g[x+1][y] == 'x':
@@ -53,15 +55,15 @@ def getAvailableNextSteps(g, x, y):
     if y+1 < len(g[x]) and g[x][y+1] == 'x':
         yield (x, y+1)
 
-def printGrid(g, success=0):
+def print_grid(g, success=0):
     if success:
-	print '---SUCCESS---'
+        print '---SUCCESS---'
     for y in range(0, len(g[0])):
         for x in range(0, len(g)):
             print '%s ' % g[x][y],
         print ""
     if success:
-	print '---SUCCESS---'
+        print '---SUCCESS---'
     print ""
 
-print '=== Total number of paths: %s' % numPathsWithPathStepsInEachCell(grid)
+print '=== Total number of paths: %s' % num_paths(grid)
