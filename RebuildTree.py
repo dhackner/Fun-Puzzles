5b0bbb82dcc5ed75b6571b67dcbb00afc965dbf6
Dan Hackner <dan.hackner@gmail.com>
2011-09-24 16:31:56 -0700

    80 character spacing

--- RebuildTree.py
+++ RebuildTree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-"""Given two arrays representing the inorder and postorder traversals of a binary tree with unique elements, rebuild the tree."""
+"""Given two arrays representing the inorder and postorder traversals of a binary tree 
+with unique elements, rebuild the tree."""
 
 __author__ = "Dan Hackner"
 __maintainer__ = "Dan Hackner"
@@ -42,12 +43,16 @@ def recreate_tree(inorder, postorder):
         return None
     splitting_letter = postorder[-1]
     split_index = inorder.index(splitting_letter)
-    # The recursion splits inorder based on knowing that the last letter of a postorder
-    # must be a parent node. The two children come from Recursing with the split of the inorder,
-    # and a matching set of postorder. i.e. inorder=[1,2,3,4] postorder=[1,3,4,2] so we know that
-    # the 2 is a parent; when we split the inorder on 2 we learn that 1 is a left child and [3,4]
-    # are a right child. We can thus recurse with ([1], [1]) and ([3,4], [3,4]). Critical to
-    # skip the consumed 'splitting letter' on each (2 in this case).
-    return Node(splitting_letter, recreate_tree(inorder[0:split_index], postorder[0:split_index]), recreate_tree(inorder[split_index+1:], postorder[split_index:-1]))
+    # The recursion splits inorder based on knowing that the last letter of a
+    # postorder must be a parent node. The two children come from Recursing
+    # with the split of the inorder, and a matching set of postorder. 
+    # i.e. inorder=[1,2,3,4] postorder=[1,3,4,2] so we know that the
+    # 2 is a parent; when we split the inorder on 2 we learn that 1 is a
+    # left child and [3,4] are a right child. We can thus recurse 
+    # with ([1], [1]) and ([3,4], [3,4]). Critical to skip the consumed
+    # 'splitting letter' on each (2 in this case).
+    return Node(splitting_letter,
+            recreate_tree(inorder[0:split_index], postorder[0:split_index]),
+            recreate_tree(inorder[split_index+1:], postorder[split_index:-1]))
 
 print recreate_tree(inorder, postorder)
